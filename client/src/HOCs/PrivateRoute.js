9e038e2e9b8a7ce140a62993e85049f00f59cbe8
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2022-04-20 17:01:42 -0400

    Update PrivateRoute.js

--- client/src/HOCs/PrivateRoute.js
+++ client/src/HOCs/PrivateRoute.js
@@ -1,11 +1,23 @@
 import React, { useContext } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import withContext from '../contexts/Context';
 import Forbidden from '../components/Forbidden';
 import { Context } from '../contexts/Context';
+import CreateCourse from '../components/CreateCourse';
+import UpdateCourse from '../components/UpdateCourse';
 
-export default function PrivateRoute( ) {
+export default function PrivateRoute() {
+      const navigate = useNavigate();
       const context = useContext(Context);
-      console.log(context)
-      return 'hi'
-    }
-
+      let { id } = useParams();
+      
+      if (context.authenticatedUser === null || context.authenticatedUser === undefined) {
+            return <Forbidden context={context}/>
+      }
+      else if (window.location.href === `http://localhost:3000/courses/create`) {
+            return <CreateCourse context={context} navigate={navigate}/>
+      }
+      else if (window.location.href === `http://localhost:3000/courses/${id}/update`) {
+            return <UpdateCourse context={context} navigate={navigate}/>
+      }
+}
