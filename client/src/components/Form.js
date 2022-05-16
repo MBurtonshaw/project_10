89a3ac6f2ef95cd3d7be193441b4a1561020b4ef
= <=>
2022-05-16 16:29:33 -0400

    form comments

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -10,17 +10,20 @@ export default function Form ( props ) {
 
   function handleSubmit( event ) {
     event.preventDefault();
+    //submit() will be defined in whichever component imports the Form component
     submit();
   }
 
   function handleCancel( event ) {
     event.preventDefault();
+    //cancel() will be defined in whichever component imports the Form component
     cancel();
   }
 
   return (
     <div>
       <form onSubmit={ handleSubmit }>
+        {/* Elements are declared in the component importing this one */}
         { elements() }
         <div className="pad-bottom">
           <button className="button" type="submit">{ submitButtonText }</button>
