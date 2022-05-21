cc933339f2b660e5a694b02dc09f8d7252b8d480
= <=>
2022-05-21 16:44:31 -0400

    done

--- client/src/HOCs/PrivateRoute.js
+++ client/src/HOCs/PrivateRoute.js
@@ -1,8 +1,6 @@
-import React, { useContext, useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { useNavigate, useParams, Outlet, Navigate } from 'react-router-dom';
 import { Context } from '../contexts/Context';
-import CreateCourse from '../components/CreateCourse';
-import UpdateCourse from '../components/UpdateCourse';
 
 export default function PrivateRoute() {
       const navigate = useNavigate();
@@ -10,16 +8,9 @@ export default function PrivateRoute() {
   
       let { id } = useParams();
 
-      useEffect(()=>{
-            if (context.authenticatedUser === null) {
-                  navigate('/signin');
-            }
-      });
-
-      if (window.location.href === `http://localhost:3000/courses/create`) {
-            return <CreateCourse context={context} navigate={navigate}/>
-      }
-      if (window.location.href === `http://localhost:3000/courses/${id}/update`) {
-            return <UpdateCourse context={context} navigate={navigate}/>
-      }
+      if (context.authenticatedUser === null) {
+            return <Navigate to="/signin" />;
+          } else {
+            return <Outlet />;
+          }
 }
